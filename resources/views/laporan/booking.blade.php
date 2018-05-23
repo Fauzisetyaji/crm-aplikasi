5e8e968fe601cbf0259f3862620027e26af98363
Fauzisetyaji <setyajfauzi@gmail.com>
2018-05-23 14:59:51 +0700

    edit laporan rekapitulsi booking service

--- resources/views/laporan/booking.blade.php
+++ resources/views/laporan/booking.blade.php
@@ -44,7 +44,7 @@
                             <img width="120px" src="{{ public_path(). '/img/astrido.png' }}">
                         </td>
                         <td width="50%" align="center">
-                            <h2>Rekapitulasi Booking Service</h2>
+                            <h2>Laporan Rekapitulasi Booking Service</h2>
                         </td>
                         <td width="25%" align="right">
                             <h3>
