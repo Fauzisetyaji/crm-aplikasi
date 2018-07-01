87ec29785cadae3b7537c1f242eb9df13e588fb6
EkaPutra <eka@flipbox.co.id>
2018-07-01 16:37:05 +0700

    [UPDATE] session expired user guest

--- resources/views/auth/login-guest.blade.php
+++ resources/views/auth/login-guest.blade.php
@@ -110,7 +110,7 @@
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
-                                    Register
+                                    Masuk
                                 </button>
                             </div>
                         </div>
